0c01829c665f19e8ca51f953539f6a8201652a51
CHFR-wide <CHFR-wide@users.noreply.github.com>
2025-08-23 14:08:32 +0200

    ease_copy v1.0.4

--- ease_copy.lua
+++ ease_copy.lua
@@ -1,6 +1,7 @@
 -- Easecopy v1.0.3
 
 -- Changelog:
+-- v1.0.4 : Internal refactors + adds an "apply inverted" button
 -- v1.0.3 : Makes the script composition-agnostic
 -- v1.0.2 : Fixes an issue where number-only ease names would crash the script
 
@@ -10,62 +11,66 @@
 
 local ui = fu.UIManager
 local disp = bmd.UIDispatcher(ui)
-local width,height = 275,300
-local positionX,positionY = 800,400
+local width, height = 275, 300
+local positionX, positionY = 800, 600
 local currentComp = fu:GetCurrentComp()
 
 win = disp:AddWindow({
-	ID = 'MyWin',
-	TargetID = 'MyWin',
-	WindowTitle = 'Ease Copy',
-  Geometry = {positionX, positionY, width, height},
-	Spacing = 0,
-  ui:VGroup{
+  ID = 'MyWin',
+  TargetID = 'MyWin',
+  WindowTitle = 'Ease Copy',
+  Geometry = { positionX, positionY, width, height },
+  Spacing = 0,
+  ui:VGroup {
     ID = 'root',
-    ui:Label{
+    ui:Label {
       Weight = 0,
       Text = 'Apply',
-      Alignment = {AlignHCenter = true},
+      Alignment = { AlignHCenter = true },
     },
-    ui:HGroup{
+    ui:HGroup {
       Weight = 0,
-      ui:Label    { Weight = 0.5, Text = 'Ease', },
+      ui:Label { Weight = 0.5, Text = 'Ease', },
       ui:ComboBox { Weight = 2, ID = 'qEase', Text = '', },
     },
-    ui:HGroup{
+    ui:HGroup {
       Weight = 0,
-      ui:Label    { Weight = 0.5, Text = 'Target', },
+      ui:Label { Weight = 0.5, Text = 'Target', },
       ui:ComboBox { Weight = 2, ID = 'qTargetProp', Text = '', },
     },
-    ui:Button { Weight=0, ID = 'qApplyBtn', Text = 'Apply ease' },
-    ui:TabBar{},
-    ui:Label{
+    ui:HGroup {
+      Weight = 0,
+      ui:Button { ID = 'qApplyBtn', Text = 'Apply' },
+      ui:Button { ID = 'qApplyInvertedBtn', Text = 'Apply (inverted)' },
+    },
+    ui:TabBar {},
+    ui:Label {
       Weight = 0,
       Text = 'Save',
-      Alignment = {AlignHCenter = true},
+      Alignment = { AlignHCenter = true },
     },
-    ui:HGroup{
+    ui:HGroup {
       Weight = 0,
       ui:LineEdit { ID = 'qSaveEaseText' },
       ui:Button { ID = 'qSaveBtn', Text = 'Save ease' },
     },
-    ui:TabBar{},
-    ui:Label{
+    ui:TabBar {},
+    ui:Label {
       Weight = 0,
       Text = 'Delete',
-      Alignment = {AlignHCenter = true},
+      Alignment = { AlignHCenter = true },
     },
-    ui:HGroup{
+    ui:HGroup {
       Weight = 0,
-      ui:Button { 
-        Weight = 0.5, 
-        ID = 'qDeleteOne', 
-        Text = 'Selected ease' 
+      ui:Button {
+        Weight = 0.5,
+        ID = 'qDeleteOne',
+        Text = 'Selected ease'
       },
-      ui:Button { 
-        Weight = 0.5, 
-        ID = 'qDeleteAll', 
-        Text = 'All eases' 
+      ui:Button {
+        Weight = 0.5,
+        ID = 'qDeleteAll',
+        Text = 'All eases'
       },
     },
 
@@ -80,7 +85,7 @@ itm = win:GetItems()
 notify = ui:AddNotify('Comp_Activate_Tool')
 
 function win.On.MyWin.Close(ev)
-	disp:ExitLoop()
+  disp:ExitLoop()
 end
 
 function disp.On.Comp_Activate_Tool(ev)
@@ -92,7 +97,16 @@ function win.On.qApplyBtn.Clicked(ev)
   local targetProp = itm.qTargetProp.CurrentText
 
   if (presetName ~= '') then
-    EaseCopy(presetName, targetProp)
+    PasteEase(presetName, targetProp)
+  end
+end
+
+function win.On.qApplyInvertedBtn.Clicked(ev)
+  local presetName = itm.qEase.CurrentText
+  local targetProp = itm.qTargetProp.CurrentText
+
+  if (presetName ~= '') then
+    PasteEase(presetName, targetProp, true)
   end
 end
 
@@ -101,7 +115,7 @@ function win.On.qSaveBtn.Clicked(ev)
   local targetProp = itm.qTargetProp.CurrentText
 
   if (presetName ~= '') then
-    if EaseCopy(presetName, targetProp, true) then
+    if CopyEase(presetName, targetProp) then
       itm.qSaveEaseText.Text = ''
       ReloadEaseComboBox(presetName)
     end
@@ -109,6 +123,9 @@ function win.On.qSaveBtn.Clicked(ev)
 end
 
 function win.On.qDeleteOne.Clicked(ev)
+  local confirmClear = currentComp:AskUser("Delete this ease?", {})
+  if not confirmClear then return end
+
   local presetName = itm.qEase.CurrentText
   fusion:SetData("easeCopy.presets." .. presetName, nil)
   ReloadEaseComboBox()
@@ -118,6 +135,7 @@ end
 function win.On.qDeleteAll.Clicked(ev)
   local confirmClear = currentComp:AskUser("Delete all eases?", {})
   if not confirmClear then return end
+
   fusion:SetData("easeCopy", nil)
   ReloadEaseComboBox()
   print('All eases have been deleted.')
@@ -126,14 +144,11 @@ end
 -- DISPLAY UPDATE --
 
 function ReloadEaseComboBox(newSelected)
-  dump('reloading')
   local savedEases = fusion:GetData("easeCopy.presets")
   local presets = {}
 
-
-
-  if savedEases then;
-    presets = GetKeys(fusion:GetData("easeCopy.presets"));
+  if savedEases then
+    presets = GetKeys(fusion:GetData("easeCopy.presets"))
   end
 
   itm.qEase:Clear()
@@ -144,7 +159,6 @@ function ReloadEaseComboBox(newSelected)
   if newSelected ~= '' then
     itm.qEase:SetCurrentText(newSelected)
   end
-
 end
 
 function ReloadTargetComboBox()
@@ -152,7 +166,7 @@ function ReloadTargetComboBox()
 
   itm.qTargetProp:Clear()
   itm.qTargetProp:AddItem('ALL')
-  for _, target in pairs(FindEligibleInputs(currentComp:GetToolList(true))) do
+  for _, target in pairs(FindAllAnimatedInputsForTools(currentComp:GetToolList(true))) do
     itm.qTargetProp:AddItem(target)
   end
 end
@@ -161,83 +175,51 @@ end
 -- MAIN LOGIC --
 ----------------
 
+function CopyEase(presetName, targetProp)
+  local adjacentKeyframes = GetAllAdjacentKeyframes(targetProp)
+  local adjacentKeyframe = adjacentKeyframes[1]
 
-function EaseCopy(presetName, targetProp, copy)
-  currentComp:StartUndo("EaseCopy")
-  currentComp:Lock()
-  for k,v in pairs(currentComp:GetToolList(true)) do
-    local endExecutionEarly = EaseCopyTool(v, presetName, targetProp, copy)
-    if (endExecutionEarly) then
-      currentComp:Unlock()
-      currentComp:EndUndo(true)
-      return true
-    end
-  end
-  currentComp:Unlock()
-  currentComp:EndUndo(true)
-end
-
-function EaseCopyTool(tool, presetName, targetProp, copy)
-  for k,v in pairs(tool:GetInputList()) do
-    local endExecutionEarly = EaseCopyInput(v, presetName, targetProp, copy)
-    if (endExecutionEarly) then
-      return true
-    end
-  end
-end
-
-function EaseCopyInput(input, presetName, targetProp, copy)
-  if not IsViableInput(input) then return end
-  
-  local inputTool = GetTool(input)
-
-  if not inputTool then; return; end
-
-  if (IsModifier(inputTool) and not IsBezierSpline(inputTool)) then
-    return EaseCopyTool(inputTool, presetName, targetProp, copy)
-  end
-
-  if not IsTargetInput(input, targetProp) then return end
-  
-  local keyframes = inputTool:GetKeyFrames()
-
-  if not keyframes then; return; end
-
-  local adjacentKeyframes = GetAdjacentKeyframes(keyframes)
-
-  if not adjacentKeyframes then
+  if not adjacentKeyframe then
     return
   end
 
-  print("Found valid property: " .. input:GetAttrs().INPS_Name)
-  if copy then
-    CopyEase(presetName, adjacentKeyframes)
-    return true
-  else
-    local hardReplace = input:GetAttrs().INPS_ID ~= "Displacement"
+  local normalized = NormalizeKeyframePairHandles(adjacentKeyframe.keyframes)
 
-    PasteEase(inputTool, presetName, adjacentKeyframes, hardReplace)
-  end
-
-end
-
-function CopyEase(presetName, adjacentKeyframes)
-  local normalized = NormalizeKeyframePairHandles(adjacentKeyframes)
   print("copying ease as " .. presetName)
   fusion:SetData("easeCopy.presets." .. presetName, normalized)
+
+  return true
 end
 
-function PasteEase(tool, presetName, adjacentKeyframes, hardReplace)
+function PasteEase(presetName, targetProp, inverted)
   local ease = fusion:GetData("easeCopy.presets." .. presetName)
-  if ease then
-    print("pasting ease preset " .. presetName)
-    local denormalized = DenormalizeKeyframePairHandles(adjacentKeyframes, ease)
+  if not ease then
+    return
+  end
+
+  if inverted == true then
+    local RH = ease.RH
+    local LH = ease.LH
+    ease.RH = { -LH[1], -LH[2] }
+    ease.LH = { -RH[1], -RH[2] }
+  end
+
+  print("pasting ease preset " .. presetName .. (inverted and ' (inverted)' or ''))
+
+  currentComp:StartUndo("EaseCopy")
+  currentComp:Lock()
+
+  for _, v in pairs(GetAllAdjacentKeyframes(targetProp)) do
+    local tool = GetTool(v.input)
     local oldKf = tool:GetKeyFrames()
+
+    local denormalized = DenormalizeKeyframePairHandles(v.keyframes, ease)
     local newKf = PatchExistingKeyFrames(oldKf, denormalized)
 
+    local hardReplace = v.input:GetAttrs().INPS_ID ~= "Displacement"
+
     if hardReplace then
-      tool:DeleteKeyFrames(currentComp:GetAttrs().COMPN_GlobalStart, currentComp:GetAttrs().COMPN_GlobalEnd)
-      tool:SetKeyFrames(newKf, false)
+      tool:SetKeyFrames(newKf, true)
     else
       ShowDisplacementWarning()
       tool:SetKeyFrames(newKf, false)
@@ -246,14 +228,18 @@ function PasteEase(tool, presetName, adjacentKeyframes, hardReplace)
       tool:SetKeyFrames(newKf, false)
     end
   end
+
+  currentComp:Unlock()
+  currentComp:EndUndo(true)
 end
 
 function ShowDisplacementWarning()
   if fusion:GetData('easeCopy.displacementWarning.doNotShow') == true then return end
-  local warnMessage = 'Pasting eases on a Displacement property can give inconsistent results, if it doesn\'t work as intended, please use an XY path modifier instead.'
-  local warnText = {"Message", Name="Message", "Text", ReadOnly = true, Wrap = true, Default = warnMessage, Lines = 5}
-  local checkBox = {"DoNotShowAgain", Name="Don\'t show this message again", "Checkbox"}
-  local dialog = currentComp:AskUser("Warning", {warnText, checkBox})
+  local warnMessage =
+  'Pasting eases on a Displacement property can give inconsistent results, if it doesn\'t work as intended, please use an XY path modifier instead.'
+  local warnText = { "Message", Name = "Message", "Text", ReadOnly = true, Wrap = true, Default = warnMessage, Lines = 5 }
+  local checkBox = { "DoNotShowAgain", Name = "Don\'t show this message again", "Checkbox" }
+  local dialog = currentComp:AskUser("Warning", { warnText, checkBox })
   dump(dialog)
   if dialog and dialog.DoNotShowAgain == 1 then
     dump('Saving preferences')
@@ -261,11 +247,62 @@ function ShowDisplacementWarning()
   end
 end
 
+function GetAllAdjacentKeyframes(targetProp)
+  local inputKeyframeDataTable = {}
+  for k, tool in pairs(currentComp:GetToolList(true)) do
+    local keyframesInTool = GetAdjacentKeyframesForTool(tool, targetProp)
+    for _, kf in pairs(keyframesInTool) do
+      table.insert(inputKeyframeDataTable, kf)
+    end
+  end
+  return inputKeyframeDataTable
+end
+
+function GetAdjacentKeyframesForTool(tool, targetProp)
+  local inputKeyframeDataTable = {}
+  for k, v in pairs(tool:GetInputList()) do
+    local inInput = GetAdjacentKeyframesForInput(v, targetProp)
+    if (inInput) then
+      table.insert(inputKeyframeDataTable, { input = v, keyframes = inInput })
+    end
+  end
+  return inputKeyframeDataTable
+end
+
+function GetAdjacentKeyframesForInput(input, targetProp)
+  if not IsViableInput(input) then return end
+
+  local inputTool = GetTool(input)
+
+  if not inputTool then
+    return
+  end
+
+  if (IsModifier(inputTool) and not IsBezierSpline(inputTool)) then
+    return GetAdjacentKeyframesForTool(inputTool, targetProp)
+  end
+
+  if not IsTargetInput(input, targetProp) then return end
+
+  local keyframes = inputTool:GetKeyFrames()
+
+  if not keyframes then
+    return
+  end
+
+  local adjacentKeyframes = GetAdjacentKeyframes(keyframes)
+  if not adjacentKeyframes then
+    return
+  end
+
+  return adjacentKeyframes
+end
+
 ----------------------------------
 -- INPUT PARSING AND VALIDATION --
 ----------------------------------
 
-function FindEligibleInputs(tools)
+function FindAllAnimatedInputsForTools(tools)
   local eligibleInputs = {}
   for _, tool in pairs(tools) do
     for _, input in pairs(tool:GetInputList()) do
@@ -274,9 +311,9 @@ function FindEligibleInputs(tools)
         if IsModifier(inputTool) then
           if IsBezierSpline(inputTool) then
             local savedTarget = tool:GetAttrs().TOOLS_Name .. ":" .. input:GetAttrs().INPS_ID
-            table.insert(eligibleInputs, savedTarget  )
+            table.insert(eligibleInputs, savedTarget)
           else
-            for _, v in ipairs(FindEligibleInputs({inputTool})) do
+            for _, v in ipairs(FindAllAnimatedInputsForTools({ inputTool })) do
               table.insert(eligibleInputs, v)
             end
           end
@@ -297,8 +334,8 @@ function IsTargetInput(input, targetProp)
 
   t = Split(targetProp, ':')
 
-  dump(input:GetAttrs().INPS_ID)
-  dump(input:GetTool():GetAttrs().TOOLS_Name == t[1] and input:GetAttrs().INPS_ID == t[2])
+  -- dump(input:GetAttrs().INPS_ID)
+  -- dump(input:GetTool():GetAttrs().TOOLS_Name == t[1] and input:GetAttrs().INPS_ID == t[2])
 
   return input:GetTool():GetAttrs().TOOLS_Name == t[1] and input:GetAttrs().INPS_ID == t[2]
 end
@@ -308,7 +345,7 @@ function IsModifier(tool)
   local regModifiers = fusion:GetRegList(fusion.CT_Modifier)
   local toolAttrs = tool:GetAttrs()
 
-  for _,v in pairs(regModifiers) do
+  for _, v in pairs(regModifiers) do
     if v:GetAttrs().REGS_ID == toolAttrs.TOOLS_RegID then
       return true
     end
@@ -333,11 +370,10 @@ end
 
 
 function GetAdjacentKeyframes(keyframes)
-
   local closestLeft = nil
   local closestRight = nil
 
-  for k,v in pairs(keyframes) do
+  for k, v in pairs(keyframes) do
     if k <= currentComp.CurrentTime and (closestLeft == nil or k > closestLeft) then
       closestLeft = k
     end
@@ -347,10 +383,9 @@ function GetAdjacentKeyframes(keyframes)
   end
 
   if (closestLeft and closestRight) then
-    return {[closestLeft] = keyframes[closestLeft], [closestRight] = keyframes[closestRight]}
+    return { [closestLeft] = keyframes[closestLeft], [closestRight] = keyframes[closestRight] }
   end
 end
-
 
 function NormalizeKeyframePairHandles(adjacentKeyframes)
   local tLeft, hLeft, tRight, hRight = SortAdjacentFrames(adjacentKeyframes)
@@ -358,7 +393,9 @@ function NormalizeKeyframePairHandles(adjacentKeyframes)
   local timeDiff = tRight - tLeft
   local valueDiff = hRight[1] - hLeft[1]
 
-  if valueDiff == 0 then; return nil; end
+  if valueDiff == 0 then
+    return nil
+  end
 
   local RH = hLeft.RH
   local LH = hRight.LH
@@ -382,7 +419,7 @@ function DenormalizeKeyframePairHandles(adjacentKeyframes, normalized)
   adjacentKeyframes[tRight].LH = { LH[1] * timeDiff, LH[2] * valueDiff }
   adjacentKeyframes[tLeft].Flags = { RH[1] * timeDiff, RH[2] * valueDiff }
   adjacentKeyframes[tRight].Flags = { LH[1] * timeDiff, LH[2] * valueDiff }
-  
+
   return adjacentKeyframes
 end
 
@@ -411,37 +448,41 @@ end
 -- HELPER FUNCTIONS --
 ----------------------
 
-function PairsByKeys (t, f)
+function PairsByKeys(t, f)
   local a = {}
   for n in pairs(t) do table.insert(a, tostring(n)) end
   table.sort(a, f)
   local i = 0
-  local iter = function ()
+  local iter = function()
     i = i + 1
-    if a[i] == nil then return nil
-    else return a[i], t[a[i]]
+    if a[i] == nil then
+      return nil
+    else
+      return a[i], t[a[i]]
     end
   end
   return iter
 end
 
 function GetKeys(t)
-  if t == nil then; return; end 
-  local keys={}
+  if t == nil then
+    return
+  end
+  local keys = {}
 
-  for key,_ in PairsByKeys(t) do
+  for key, _ in PairsByKeys(t) do
     table.insert(keys, key)
   end
   return keys
 end
 
-function Split (inputstr, sep)
+function Split(inputstr, sep)
   if sep == nil then
-     sep = "%s"
+    sep = "%s"
   end
-  local t={}
-  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-     table.insert(t, str)
+  local t = {}
+  for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    table.insert(t, str)
   end
   return t
 end
